2be215a8fb762eaed00876c0c2b22df14212def3
enbriziare <enzo.briziarelli@gmail.com>
2021-11-24 18:23:09 +0100

    #1 Duplicated service-front ++ full dockerized

---
+++ frontends/example-bis/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import RestService from './RestService.js';
+import React from 'react';
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {message: ''};
+  }
+  async componentDidMount() {
+    let message = await RestService.get()
+    this.setState({message:message });
+  }
+  render(){
+    return (
+        <div className="App">
+          <p>{this.state.message}</p>
+        </div>
+    );
+  }
+}
+
---
+++ services/example-bis/README.md
@@ -0,0 +1,31 @@
+# Example(bis) service
+
+## Requirements
+
+- Install the dependencies :
+
+```bash
+npm install
+```
+
+## Development
+
+- To run the service :
+
+```bash
+npm run start:dev
+```
+
+## Production
+
+- To build the service :
+
+```bash
+npm run build
+```
+
+- To run the service :
+
+```bash
+npm run start:prod
+```
