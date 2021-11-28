0f08514b07745589b6244510c5cd44bdb17cf8ff
Marsili <sylvain.marsili@gmail.com>
2021-11-28 22:45:11 +0100

    #4 transform example-bis to single-spa-react-app

---
+++ frontends/example-bis/webpack.config.js
@@ -0,0 +1,26 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react");
+const webpack = require('webpack');
+
+const Dotenv = require('dotenv').config({ path: './.env' });
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "ter",
+    projectName: "example-bis",
+    webpackConfigEnv,
+    argv,
+  });
+
+  return merge(defaultConfig, {
+    plugins:[
+      new webpack.DefinePlugin({
+        "process.env": JSON.stringify(process.env)
+      }),
+        ],
+    devServer: {
+      port: process.env.PORT
+    },
+   
+    // modify the webpack config however you'd like to by adding to this object
+  });
+};
