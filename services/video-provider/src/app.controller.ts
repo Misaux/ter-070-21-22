c66947105e6db114966393554a94bb93dfb40ce5
Marsili <sylvain.marsili@gmail.com>
2021-12-08 12:07:24 +0100

    #8 add provider for video

---
+++ services/video-provider/src/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import { AppService } from './app.service';
+import {Response,Request} from 'express'
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getVideo(@Req() request: Request,
+  @Res() response: Response,) {
+    this.appService.getVideo(request, response);
+  }
+}
